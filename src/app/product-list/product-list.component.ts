2ce5719f39ca02a315351d4c80dd877ad4315b84
eziokhoi <trinhminhkhoi619@gmail.com>
2020-04-24 16:20:14 +0700

    Update
    Update Folder

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('Công Văn đã được chuyển giao');
+  }
+  save() {
+    window.alert('Công Văn đã được lưu');
+  }
+  delete() {
+    window.alert('Công Văn đã được xóa');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    name: 'Công Văn 1',
+    imp: 1,
+    description: 'Công văn quan trọng gủi cho bộ quốc phòng',
+    date: '20/09/1997',
+  },
+  {
+    name: 'Công Văn 2',
+    imp: 0,
+    description: 'Công văn normal',
+    date: '12/12/1997',
+  },
+  {
+    name: 'Công Văn 3',
+    imp: 0,
+    description: 'Lưu trong ngày',
+    date: '20/4/2020',
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
