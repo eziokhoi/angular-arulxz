717186504debbc1b14d4e21d8f66e332d4fd9c9a
eziokhoi <trinhminhkhoi619@gmail.com>
2020-04-28 12:00:07 +0700

    3 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,19 +1,19 @@
 export const products = [
   {
     name: 'Công Văn 1',
-    imp: 1,
+    imp: 0,
     description: 'Công văn quan trọng gủi cho bộ quốc phòng',
     date: '20/09/1997',
   },
   {
     name: 'Công Văn 2',
-    imp: 0,
+    imp: 1,
     description: 'Công văn normal',
     date: '12/12/1997',
   },
   {
     name: 'Công Văn 3',
-    imp: 0,
+    imp: 1,
     description: 'Lưu trong ngày',
     date: '20/4/2020',
   }
